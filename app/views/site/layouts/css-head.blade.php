7ee3744361eafa742c03e6d9bc0d34302761dcfd
Christian Soseman <christian.soseman@techscopellc.com>
2015-07-03 16:46:28 -0500

    Adds css file for bootstrap sticky footer

--- app/views/site/layouts/css-head.blade.php
+++ app/views/site/layouts/css-head.blade.php
@@ -1,3 +1,4 @@
 {{-- The following are CSS links that will appear in the head of the master page --}}
 <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap-theme.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/sticky-footer-navbar.css')}}">

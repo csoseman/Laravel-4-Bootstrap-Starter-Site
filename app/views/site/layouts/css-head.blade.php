2b31e1c5702e2a673d253e3524ecb473af15ce03
Christian Soseman <christian.soseman@techscopellc.com>
2015-07-03 20:29:11 -0500

    Moves login style div CSS to public folder, Adds CSS yield capability

--- app/views/site/layouts/css-head.blade.php
+++ app/views/site/layouts/css-head.blade.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap-theme.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/sticky-footer-navbar.css')}}">
+@yield('css-top')

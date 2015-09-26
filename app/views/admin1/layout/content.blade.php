596a1a100b74182477b434857adb3a1883835dff
Christian Soseman <christian.soseman@techscopellc.com>
2015-09-26 00:47:14 -0500

    Fixes #11; removes breadcrums in page header entirely

--- app/views/admin1/layout/content.blade.php
+++ app/views/admin1/layout/content.blade.php
@@ -10,10 +10,10 @@
                 <small>{{{ $title_description }}}</small>
             @endif
         </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-            <li class="active">Here</li>
-        </ol>
+        {{--<ol class="breadcrumb">--}}
+            {{--<li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>--}}
+            {{--<li class="active">Here</li>--}}
+        {{--</ol>--}}
     </section>
 
     <!-- Main content -->

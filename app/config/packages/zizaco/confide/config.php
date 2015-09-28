bc4d917562417763d4d50c37568cd8fb12a7b21e
Christian Soseman <christian.soseman@techscopellc.com>
2015-09-28 10:58:07 -0500

    Renames login page.

--- app/config/packages/zizaco/confide/config.php
+++ app/config/packages/zizaco/confide/config.php
@@ -47,7 +47,7 @@ return array(
     |
     |
     */
-    'login_form' =>             'site.partials.user.login_combined',
+    'login_form' =>             'site.partials.user.login',
     'signup_form' =>            'site.partials.user.signup',
     'forgot_password_form' =>   'site.partials.user.forgot_password',
     'reset_password_form' =>    'site.partials.user.reset_password',
